7ab44cd0d69abeba8831f01717151453326544a2
= <=>
2023-08-27 22:54:21 +0400

    make responsive

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import { Toaster } from "react-hot-toast";
 import "../styles/globals.css";
 import { Layout } from "../components";
 import { StateContext } from "../context/StateContext";
-//asd
 export default function App({ Component, pageProps }) {
   return (
     <StateContext>
